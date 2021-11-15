c31774b21e9a86ad7621bbb839dc0fb7385a182c
Yohan Lopes <yohanhxh@gmail.com>
2021-11-14 23:45:05 -0300

    updanting get carts

--- src/helpers/cart.js
+++ src/helpers/cart.js
@@ -3,8 +3,9 @@ import connection from "../database/connection.js";
 async function getCartProducts(cartId) {
     const cart = await connection.query(
         `SELECT products.id, products.name, products.description, products.image, products.stock_total as "stockTotal", products.price,
-                categories.id AS "categoryId", categories.name AS "categoryName" FROM products
+                categories.id AS "categoryId", categories.name AS "categoryName", cart_products.amount FROM products
         JOIN categories ON products.category_id = categories.id
+        JOIN cart_products ON cart_products.product_id = products.id
         WHERE products.id IN (SELECT product_id FROM cart_products WHERE cart_id = $1);`,
         [ cartId ]
     )
